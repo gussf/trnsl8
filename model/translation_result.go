3befee016bda628d245fbfa02aa50271642ea1a2
Gustavo S F <9011579-gustavosilvafranco@users.noreply.gitlab.com>
2021-06-15 19:03:26 -0300

    Improving json parse in translation

--- model/translation_result.go
+++ model/translation_result.go
@@ -1,7 +1,13 @@
 package model
 
+import "fmt"
+
 type TranslationResult struct {
 	SourceLanguage string `json:"SourceLanguageCode"`
 	TargetLanguage string `json:"TargetLanguageCode"`
 	Translation    string `json:"TranslatedText"`
+}
+
+func (t TranslationResult) String() string {
+	return fmt.Sprintf("Source Language [%s] (automatically detected)\nTarget Language [%s]\nTranslation     [%s]", t.SourceLanguage, t.TargetLanguage, t.Translation)
 }
